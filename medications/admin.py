afc74cf0a43fce4db42cb5a57d45d809435246cf
11246002 <11246002@ntub.edu.tw>
2026-05-08 11:13:00 +0800

    新增資料表

--- medications/admin.py
+++ medications/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Drug, Prescription, PrescriptionDrug, DrugWarning
 
-# Register your models here.
+admin.site.register(Drug)
+admin.site.register(Prescription)
+admin.site.register(PrescriptionDrug)
+admin.site.register(DrugWarning)
