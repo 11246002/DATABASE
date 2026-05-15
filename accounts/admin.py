cc656a342201826b4ec17c5a31790b6699cd4046
YXCCC <a0927229619@gmail.com>
2026-05-15 21:48:02 +0800

    重新設計 User/Group 資料架構並清理環境暫存檔

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group as AuthGroup # 為了讓後台取消註冊django內建的group先引入並改名
 from .models import User, Group, GroupMember
 
 admin.site.register(User)
-admin.site.register(Group)
+admin.site.register(Group)         
 admin.site.register(GroupMember)
+
+
+# 取消註冊 Django 內建的 AuthGroup
+admin.site.unregister(AuthGroup)
